9db8a71d157a9ae1161a75c033aeaee7a9082bfb
thalfor <thales.fornazari@gmail.com>
2024-10-03 16:51:27 -0300

    analog clock

--- src/pages/devProjectsEng/analogClock/index.jsx
+++ src/pages/devProjectsEng/analogClock/index.jsx
@@ -1,5 +1,5 @@
 //
-import { Container, Menu, Content } from './styles';
+import { Container, Menu, Content, Project } from './styles';
 import { Navbar } from '../../../components/navBar';
 import { Link } from 'react-router-dom';
 //
@@ -17,16 +17,47 @@ export function DevProjectsAnalogClockEng(){
           <Link to="/DevProjectsEng/DevProjectsUrlShrinkEng" style={{ textDecoration: 'none' }}>URL Shrink</Link>
         </Menu>
         <Content>
-          <h1>My Portfolio</h1>
+          <h1>Analog Clock</h1>
           <p>
-            Welcome!<br />
-            Here's my ongoing portfolio, feel free to check it out! In each page there's a description of what was done as well as the project itself.<br />
-            This page will grow as I grow as a JavaScript developer. I do not post every project that I make here, just the ones that I find more relevant at the moment.
-            <br />
-            <br />
-            You can check all of my projects at my GitHub account.
+          The code is divided in 2 parts:<br></br>
+
+          JavaScript:<br></br>
+          To get the current time there's no much of a challenge because just by using .getSeconds, .getMinutes, .getHours we have the final goal. 
+          However it's important to realize that since the clock is round we need to make sure the pointers move acording to it's time and we do that 
+          by diving the seconds, minutes, hours by it's clock ratio (60,60,12) having our full cicle of 360.<br></br>
+          CSS:<br></br>
+          With the true challenge being the CSS due to the fact it is a round analogic clock and not just simply printing the time on screen.<br></br>
+          This is the real tricky part, we need:<br></br>
+          to build a circle.<br></br>
+          to populate this circle with numbers (1-12).<br></br>
+          to fit the numbers within this circle.<br></br>
+          to build the pointers (sec, min, hours).<br></br>
+          to build the dot at the middle of the clock where the pointers will be fixed.<br></br>
+
+          Conclusion<br></br>
+          To implement this type of code one must have not only a good understanding of CSS but also a good understanding of it's logic since we 
+          needed to partition the numbers and pointers in HTML for later in JS and CSS get the clock done.<br></br>
           </p>
         </Content>
+        <Project>
+          <div class="clock">
+            <div class="hand hour" data-hour-hand></div>
+            <div class="hand minute" data-minute-hand></div>
+            <div class="hand second" data-second-hand></div>
+            <div class="number number1">1</div>
+            <div class="number number2">2</div>
+            <div class="number number3">3</div>
+            <div class="number number4">4</div>
+            <div class="number number5">5</div>
+            <div class="number number6">6</div>
+            <div class="number number7">7</div>
+            <div class="number number8">8</div>
+            <div class="number number9">9</div>
+            <div class="number number10">10</div>
+            <div class="number number11">11</div>
+            <div class="number number12">12</div>
+          </div>
+        </Project>
       </div>
     </Container>
   );
