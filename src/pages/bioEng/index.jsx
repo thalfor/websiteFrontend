f9dbd6ab0ea0658a0b8a20c3f7c959244f4c0a20
thalfor <thales.fornazari@gmail.com>
2024-10-09 11:08:53 -0300

    about me updated

--- src/pages/bioEng/index.jsx
+++ src/pages/bioEng/index.jsx
@@ -15,8 +15,8 @@ export function BioEng(){
         <div className='bio'>
           A colorblind guy just trying to better each time. <br />
           Born in Bauru but grew in Marília, São Paulo's countryside, and now living at São Paulo for over a decade. <br />
-          Once a nerd always a nerd! Passionate about learning, data-driven style, living in São Paulo for over a decade now. <br />
-          From teacher, financial analyst, team leader, dog dad, happy socks enthusiast, and now getting into Backend Develping! 
+          Once a nerd always a nerd! Passionate about learning, data-driven style, from teacher, financial analyst, team leader, dog dad, happy socks enthusiast, 
+          and now getting into Backend Develping! 
         </div>
       </div>
       <div className='images'>
