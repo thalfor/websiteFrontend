36035d06b6f8b55ce64b9f1e192078fb21ca9974
thalfor <thales.fornazari@gmail.com>
2024-10-14 09:59:05 -0300

    project website style

--- src/pages/devProjectsEng/thisWebsite/styles.js
+++ src/pages/devProjectsEng/thisWebsite/styles.js
@@ -7,15 +7,6 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100%;
   //height: 100vh;
-<<<<<<< HEAD
   margin-top: 8rem;
-=======
-  margin-top: 10rem;
-
-  pre {
-    margin: 1px solid white;
-    //background-color: white;
-  }
->>>>>>> 8a3e4da8920ec6609f051a77e7299bcc0fc18b2a
 `;
 //
