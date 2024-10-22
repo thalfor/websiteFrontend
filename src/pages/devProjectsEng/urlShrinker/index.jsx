4d5fa4884bcc758de194657451283c039bb70c40
thalfor <thales.fornazari@gmail.com>
2024-10-22 12:36:32 -0300

    updating urlShrinker project

--- src/pages/devProjectsEng/urlShrinker/index.jsx
+++ src/pages/devProjectsEng/urlShrinker/index.jsx
@@ -3,7 +3,13 @@ import { Container } from './styles';
 import { MenuProject } from '../../../components/menuProject';
 import { UrlShrinker } from '../../../components/urlShrinker';
 //
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+//
 export function DevProjectsUrlShrinkEng(){
+
+  const codeString01 = ``;
+
   return(
     <div>
     <MenuProject />
@@ -11,6 +17,12 @@ export function DevProjectsUrlShrinkEng(){
       <h1>Url Shrink</h1>
       <UrlShrinker />
       test
+      <div className='codeblock'>
+        <SyntaxHighlighter lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap', flexWrap: 'wrap'}}} style={a11yDark} wrapLines={true} language="jsx">
+          {codeString01}
+        </SyntaxHighlighter>
+      </div>
+      <br />
     </Container>
     </div>
   );
