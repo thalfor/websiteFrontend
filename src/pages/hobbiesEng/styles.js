cc541e87de356024715a2d08a76f4e241a47942e
thalfor <thales.fornazari@gmail.com>
2024-10-23 13:35:21 -0300

    fixing mobile

--- src/pages/hobbiesEng/styles.js
+++ src/pages/hobbiesEng/styles.js
@@ -3,59 +3,109 @@ import styled from 'styled-components';
 //
 export const Container = styled.div`
 
-  //border: 1px solid white;
+font-size: 1.5rem;
 
-  //padding: 8rem;
-  font-size: 2rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 2rem;
+
+h1 {
+  font-size: 3rem;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.masterHobbyBox {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.hobbyBox {
+  border: 1px solid white;
+
+  height: 24rem;
+  width: 25rem;
+  border-radius: 2rem;
+  padding: 1rem 1rem 0 1rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 3rem;
+  font-size: 1.3rem;
+}
 
-  h1 {
-    font-size: 6rem;
-  }
+.hobbyBox p:first-child {
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  padding: 0 0 .2rem 0;
+}
 
-  .title {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 1rem;
-  }
+.hobbyBox img {
+  border-radius: 10%;
+  height: 14rem;
+  margin-top: 1rem;
+}
 
-  .masterHobbyBox {
-    display: grid;
-    grid-template-columns: 40rem 40rem 40rem;
-    grid-gap: 2rem;    
-  }
-
-  .hobbyBox {
-    border: 1px solid white;
-
-    height: 34rem;
-    width: 40rem;
-    border-radius: 2rem;
-    padding: 2rem 2rem 0 2rem;
+  @media(min-width: 768px){
+    font-size: 2rem;
 
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
+    gap: 3rem;
 
-  .hobbyBox p:first-child {
-    font-size: 3rem;
-    display: flex;
-    justify-content: center;
-    padding: 0 0 1rem 0;
-  }
+    h1 {
+      font-size: 6rem;
+    }
 
-  .hobbyBox img {
-    border-radius: 10%;
-    height: 17rem;
-    margin-top: 2.5rem;
+    .title {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      gap: 1rem;
+    }
+
+    .masterHobbyBox {
+      display: grid;
+      grid-template-columns: 40rem 40rem 40rem;
+      grid-gap: 2rem;    
+    }
+
+    .hobbyBox {
+      border: 1px solid white;
+
+      height: 34rem;
+      width: 40rem;
+      border-radius: 2rem;
+      padding: 2rem 2rem 0 2rem;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .hobbyBox p:first-child {
+      font-size: 3rem;
+      display: flex;
+      justify-content: center;
+      padding: 0 0 1rem 0;
+    }
+
+    .hobbyBox img {
+      border-radius: 10%;
+      height: 17rem;
+      margin-top: 2.5rem;
+    }
   }
-  
 `;
 //
