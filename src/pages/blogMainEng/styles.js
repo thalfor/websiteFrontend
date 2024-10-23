f739f983ad0457342b240ba2be7c74a150ed2626
thalfor <thales.fornazari@gmail.com>
2024-10-23 13:38:22 -0300

    fixing mobile

--- src/pages/blogMainEng/styles.js
+++ src/pages/blogMainEng/styles.js
@@ -2,11 +2,8 @@
 import styled from 'styled-components';
 //
 export const Container = styled.div`
-  width: 100%;
-//  height: 100vh;
 
-  font-size: 2rem;
-  //padding: 8rem 8rem 1rem 8rem;
+  font-size: 1.5rem;
 
   display: flex;
   flex-direction: column;
@@ -14,19 +11,19 @@ export const Container = styled.div`
   justify-content: center;
 
   h1 {
-    font-size: 6rem;
-    margin-bottom: 3rem;
+    font-size: 3rem;
+    margin-bottom: 1rem;
   }
 
   .postsMaster {
 
-    font-size: 2rem;
+    font-size: 1.5rem;
 
-    margin-top: 4rem;
+    margin-top: 2rem;
     
-    display: grid;
-    grid-template-columns: 25rem 25rem 25rem 25rem;
-    grid-gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 
     img {
       height: 15rem;
@@ -38,7 +35,7 @@ export const Container = styled.div`
       border: 1px solid white;
       border-radius: 7%;
 
-      height: 27rem;
+      height: 23rem;
       width: 24rem;
       padding: 1rem;
 
@@ -47,7 +44,7 @@ export const Container = styled.div`
       justify-content: space-between;
       
       .datePost {
-        font-size: 1.5rem;
+        font-size: 1.2rem;
       }
     }
   }
@@ -57,5 +54,59 @@ export const Container = styled.div`
     color: ${({ theme }) => theme.colors.text};
   }
 
+
+  @media(min-width: 768px){
+    width: 100%;
+    font-size: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 6rem;
+      margin-bottom: 3rem;
+    }
+
+    .postsMaster {
+
+      font-size: 2rem;
+
+      margin-top: 4rem;
+      
+      display: grid;
+      grid-template-columns: 25rem 25rem 25rem 25rem;
+      grid-gap: 1rem;
+
+      img {
+        height: 15rem;
+        width: 22rem;
+        border-radius: 7%;
+      }
+
+      .post {
+        border: 1px solid white;
+        border-radius: 7%;
+
+        height: 27rem;
+        width: 24rem;
+        padding: 1rem;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        
+        .datePost {
+          font-size: 1.5rem;
+        }
+      }
+    }
+
+    .links {
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.text};
+    }
+  }
 `;
 //
